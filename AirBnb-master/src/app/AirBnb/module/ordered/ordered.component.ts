a198adc9083bdfc76d095321fd299a3567c1e046
Temo Gagnidze <77825373+LES-TWINS@users.noreply.github.com>
2023-01-06 18:26:11 +0400

    changes in order page

--- AirBnb-master/src/app/AirBnb/module/ordered/ordered.component.ts
+++ AirBnb-master/src/app/AirBnb/module/ordered/ordered.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -12,42 +13,46 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class OrderedComponent implements OnInit {
   addPhoneNumberForm!:FormGroup;
-  submitted=false 
+  submitted = false ;
+  reservedHotel:any = [];
 
   constructor( private location: Location, 
-    private formBuilder:FormBuilder,private http:HttpClient ) { 
+    private formBuilder:FormBuilder,private http:HttpClient,
+    private activatedRoute: ActivatedRoute ) { 
   }
 
-  goBack() {
-    this.location.back();
-  }
+
 
 
   ngOnInit(): void {
-      this.addPhoneNumberForm=this.formBuilder.group({
-       Country:['',[Validators.required,Validators]],
-       PhoneNumber:['',[Validators.required,Validators.minLength(9)]],
+    this.activatedRoute.queryParams.subscribe((info) =>{
+      this.reservedHotel = info
+      console.log(this.reservedHotel)
+    })
+      // this.addPhoneNumberForm=this.formBuilder.group({
+      //  Country:['',[Validators.required,Validators]],
+      //  PhoneNumber:['',[Validators.required,Validators.minLength(9)]],
 
-      })
+      // })
  }
 
- addPhoneNumber(){
-  this.submitted=true
+//  addPhoneNumber(){
+//   this.submitted=true
 
-  if(this.addPhoneNumberForm.invalid){
-   return
-  }
-alert("good")
+//   if(this.addPhoneNumberForm.invalid){
+//    return
+//   }
+// alert("good")
 
-   this.http.post<any>("http://localhost:3000/ordered",this.addPhoneNumberForm.value)
-   .subscribe(res=>{
-     alert("add number good")
-     this.addPhoneNumberForm.reset()
+//    this.http.post<any>("http://localhost:3000/ordered",this.addPhoneNumberForm.value)
+//    .subscribe(res=>{
+//      alert("add number good")
+//      this.addPhoneNumberForm.reset()
      
      
 
-   })
+//    })
 
   
-}
+// }
 }
