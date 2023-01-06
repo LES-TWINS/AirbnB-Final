8dcb8434058ffcebc9f9a49a210c84543121b92f
Khatuna Chaphodze <111879031+Khatuniko@users.noreply.github.com>
2023-01-06 04:29:37 +0400

    working on ordered page

--- AirBnb-master/src/app/AirBnb/module/ordered/ordered.component.ts
+++ AirBnb-master/src/app/AirBnb/module/ordered/ordered.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-ordered',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ordered.component.scss']
 })
 export class OrderedComponent implements OnInit {
+  addPhoneNumberForm!:FormGroup;
+  submitted=false 
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( private location: Location, 
+    private formBuilder:FormBuilder,private http:HttpClient ) { 
   }
 
+  goBack() {
+    this.location.back();
+  }
+
+
+  ngOnInit(): void {
+      this.addPhoneNumberForm=this.formBuilder.group({
+       Country:['',[Validators.required,Validators]],
+       PhoneNumber:['',[Validators.required,Validators.minLength(9)]],
+
+      })
+ }
+
+ addPhoneNumber(){
+  this.submitted=true
+
+  if(this.addPhoneNumberForm.invalid){
+   return
+  }
+alert("good")
+
+   this.http.post<any>("http://localhost:3000/ordered",this.addPhoneNumberForm.value)
+   .subscribe(res=>{
+     alert("add number good")
+     this.addPhoneNumberForm.reset()
+     
+     
+
+   })
+
+  
+}
 }
